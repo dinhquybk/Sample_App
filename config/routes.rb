83ca6019531c6688e465f80b67710cc607aede07
dinhquybk <nguyendinhquy94@gmail.com>
2015-12-11 17:29:08 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'sessions/new'
+
+  get 'users/new'
+
+
+  get 'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+  get  'static_pages/contact'
+  get 'signup'=>'users/new'
+   get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+    resources :users
+    resources :account_activations, only: [:edit]
+    resources :password_resets,     only: [:new, :create, :edit, :update]
+    resources :microposts,          only: [:create, :destroy]
+     resources :relationships,       only: [:create, :destroy]
+    resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+end
